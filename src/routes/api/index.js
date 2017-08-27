ff1a9f2099e04795d3185c4a27d4279910d5dbb7
Sushant Badangod <sushant.bs@gmail.com>
2017-08-27 23:08:25 +0800

    Prettier formatting

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,80 +1,86 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var request = require('superagent');
-var fetch = require('node-fetch');
-var memjs = require('memjs');
-var _ = require('lodash');
-var Promise = require('es6-promise').Promise;
-var config = require('../../../config/config.heroku');
+var request = require("superagent");
+var fetch = require("node-fetch");
+var memjs = require("memjs");
+var _ = require("lodash");
+var Promise = require("es6-promise").Promise;
+var config = require("../../../config/config.heroku");
 var serviceUrl = `${config.db.endpoint}/api`;
 
 var cacheClient = memjs.Client.create();
 var cache = {
-  get: key => (new Promise((resolve, reject) => {
-    //console.log(`Getting ${key} from cache`);
-    cacheClient.get(key, (err, val) => {
-      console.log(err, val);
-      return (err ? reject(err) : resolve(val))
-    });
-  })),
-  set: (key, value, expires) => (new Promise((resolve, reject) => {
-    //console.log(`Setting ${key} to cache with value ${value}`);
-    cacheClient.set(key, value, {expires}, (err, val) => {
-      console.log(arguments);
-      return (err ? reject(err) : resolve(val)));
-    });
-  })),
-  delete: key => (new Promise((resolve, reject) => {
-    cacheClient.delete(key, (err, val) => (err ? reject(err) : resolve(val)));
-  }))
-}
+  get: key =>
+    new Promise((resolve, reject) =>
+      cacheClient.get(key, (err, val) => {
+        return err ? reject(err) : resolve(val);
+      })
+    ),
+  set: (key, value, expires) =>
+    new Promise((resolve, reject) =>
+      cacheClient.set(key, value, { expires }, (err, val) => {
+        return err ? reject(err) : resolve(val);
+      })
+    ),
+  delete: key =>
+    new Promise((resolve, reject) =>
+      cacheClient.delete(key, (err, val) => (err ? reject(err) : resolve(val)));
+    )
+};
 
-var errorHandler = function (res) {
-  return function (err) {
+var errorHandler = function(res) {
+  return function(err) {
     if (err) {
       res.status(500).send(err);
       return;
     }
-  }
-}
+  };
+};
 //var cache;
 module.exports = router;
 
-var validate =  function (req, res, next) {
-
+var validate = function(req, res, next) {
   let token = req.headers.token;
 
   if (!token) {
-    res.status(500).send('Token not found');
+    res.status(500).send("Token not found");
     return;
   }
 
   cache
     .get(token)
-    .then((tokenResponse) => {
+    .then(tokenResponse => {
       if (tokenResponse) {
         return tokenResponse;
       } else {
         return Promise.all([
           new Promise((resolve, reject) => {
-            return fetch('https://graph.facebook.com/debug_token?access_token=1074798345983056|O6QWGRy2Sd9YAknhlkvg5qjbuJM&input_token=' + token)
+            return fetch(
+              "https://graph.facebook.com/debug_token?access_token=1074798345983056|O6QWGRy2Sd9YAknhlkvg5qjbuJM&input_token=" +
+                token
+            )
               .then(response => response.json())
               .then(respJson => resolve(respJson))
               .catch(err => reject(err));
-            })
-          ,
+          }),
           new Promise((resolve, reject) => {
-            return fetch('https://graph.facebook.com/me?fields=email&access_token=' + token)
+            return fetch(
+              "https://graph.facebook.com/me?fields=email&access_token=" + token
+            )
               .then(response => response.json())
               .then(respJson => resolve(respJson))
               .catch(err => reject(err));
-            })
-        ])
+          })
+        ]);
       }
     })
-    .then((response) => ((response instanceof Array) ? _.extend(response[0], response[1]) : response))
-    .then((responseJson) => {
-
+    .then(
+      response =>
+        response instanceof Array
+          ? _.extend(response[0], response[1])
+          : response
+    )
+    .then(responseJson => {
       let error = responseJson.data && responseJson.data.error;
       if (error) {
         if (error.code === 190) {
@@ -83,7 +89,7 @@ var validate =  function (req, res, next) {
             cache.delete(token);
           }
 
-          res.status(200).send({status: 'ERROR', error: error});
+          res.status(200).send({ status: "ERROR", error: error });
         }
       }
 
@@ -92,8 +98,8 @@ var validate =  function (req, res, next) {
         let currTime = new Date().valueOf();
         cache.set(
           token,
-          _.extend({cached: true}, responseJson),
-          (Number(responseJson.expiresAt) - Math.ceil(currTime / 1000))
+          _.extend({ cached: true }, responseJson),
+          Number(responseJson.expiresAt) - Math.ceil(currTime / 1000)
         );
       }
 
@@ -104,50 +110,46 @@ var validate =  function (req, res, next) {
       req.user.set(req, responseJson);
       next();
     })
-    .catch((err) => {
+    .catch(err => {
       console.log("Token validation error: ", err);
       res.status(500).send(err);
     });
 };
 
-router.get('/user/profile', validate, (req, res, next) => {
-
+router.get("/user/profile", validate, (req, res, next) => {
   var oAuthUserObj = req.user.get(req);
   var emailId = oAuthUserObj.email;
 
-  fetch(serviceUrl + '/user?emailId=' + emailId)
+  fetch(serviceUrl + "/user?emailId=" + emailId)
     .then(response => response.json())
     .then(respJson => {
       cache.set(emailId, respJson[0]);
       res.send(respJson);
     })
-    .catch(errorHandler(res))
+    .catch(errorHandler(res));
 });
 
-router.get('/user/feed', validate, (req, res, next) => {
-
+router.get("/user/feed", validate, (req, res, next) => {
   var oAuthUserObj = req.user.get(req),
     emailId = oAuthUserObj.email;
 
   cache
     .get(emailId)
-    .then((userProfile) => {
+    .then(userProfile => {
       console.log(userProfile);
       if (userProfile) {
-        fetch(serviceUrl + '/data/user/feed',
-          {
-            method: 'POST',
-            body: JSON.stringify({
-              personId: userProfile.person._id
-            }),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
+        fetch(serviceUrl + "/data/user/feed", {
+          method: "POST",
+          body: JSON.stringify({
+            personId: userProfile.person._id
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
           .then(response => response.json())
           .then(respJson => res.send(respJson))
           .catch(errorHandler(res));
-
       } else {
         res.status(403).send();
       }
@@ -155,14 +157,13 @@ router.get('/user/feed', validate, (req, res, next) => {
     .catch(errorHandler(res));
 });
 
-router.get('/gig/details', validate, (req, res, next) => {
+router.get("/gig/details", validate, (req, res, next) => {
   var oAuthUserObj = req.user.get(req),
     emailId = oAuthUserObj.email;
 
   console.log(emailId);
-  cache.get(emailId)
-    .then((userProfile) => {
-      console.log(userProfile);
-      return res.send({imFollowing: true, myStatus: 'yes'})
-    })
+  cache.get(emailId).then(userProfile => {
+    console.log(userProfile);
+    return res.send({ imFollowing: true, myStatus: "yes" });
+  });
 });
